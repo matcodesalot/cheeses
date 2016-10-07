c24d367fafe5752ecdca8c6792ee8648dd98c7fc
Lavie <lavie.ruan@gmail.com>
2016-10-07 18:36:37 +0000

    Add ReactDom render for CheeseList with array as a prop

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,21 @@
 import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {CheeseList} from './components/cheese-list';
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
+
+document.addEventListener('DOMContentLoaded', function(){
+    ReactDOM.render(
+        <CheeseList 
+            cheeses={
+                [
+                    "Bath Blue",
+                    "Barkham Blue",
+                    "Buxton Blue"
+                ]
+            }
+        />, 
+        document.getElementById('app')
+    );
+});
