7db05a86709e34c09f2bacc207ee5666a4099e93
Mathew Johns <reallymathew@gmail.com>
2016-10-07 16:46:09 +0000

    add cheese-list

---
+++ client/js/components/cheese-list.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+let CheeseList = function(props) {
+    let cheeses = props.cheeses
+    //TODO: map through cheeses
+    return (
+        <ul>
+            
+        </ul>
+    );
+};
