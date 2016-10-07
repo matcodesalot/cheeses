81af10b094b5fb818090781a3aa3e45328829658
Lavie <lavie.ruan@gmail.com>
2016-10-07 18:19:47 +0000

    Edit CheeseList component in client

--- client/js/components/cheese-list.js
+++ client/js/components/cheese-list.js
@@ -2,10 +2,11 @@ import React from "react";
 
 let CheeseList = function(props) {
     let cheeses = props.cheeses
-    //TODO: map through cheeses
     return (
         <ul>
-            
+            <li> {cheeses.map(cheese => cheese)} </li>
         </ul>
     );
 };
+
+export {CheeseList};
